0d12824666e08b1b0d675300f3d9967aa67fa580
= <=>
2021-02-18 19:33:05 +0700

    Membuat AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,24 @@ void main() {
 class HomePage extends StatelessWidget {
   build(context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey,
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(text: 'Berita Hari Ini'),
+                Tab(text: 'Pertandingan Terbaru')
+              ],),
+            backgroundColor: Colors.grey,
           title: Text('MyApp')
+          ),
+          body: TabBarView(
+            children: [
+              Center(child: Text('Berita Baru')),
+              Center(child: Text('Pertandingan Baru'))
+            ],)
         ),
       )
     );
